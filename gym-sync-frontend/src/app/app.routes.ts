b6205f4ce258ea92c60f4d81ad164f6984f87c98
Paweł Heleniak <pawel.heleniak01@gmail.com>
2025-10-19 12:55:03 +0200

    Update angular, add panel, menu, fixed router

--- gym-sync-frontend/src/app/app.routes.ts
+++ gym-sync-frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { UserLayout } from './layout/user-layout/user-layout';
 import { TrainingSession } from './features/user/training-session/training-session';
 import { TrainingReport } from './features/user/training-report/training-report';
 import { TrainingPlanBuilder } from './features/user/training-plan-builder/training-plan-builder';
+import { HomePanel } from './features/user/home-panel/home-panel';
 
 export const routes: Routes = [
   {
     path: '',
     component: UserLayout,
     children: [
+      { path: 'panel', component: HomePanel },
       { path: 'trening', component: TrainingSession },
       { path: 'raport', component: TrainingReport },
       { path: 'kreator-planu', component: TrainingPlanBuilder },
---
+++ gym-sync-frontend/src/app/features/user/home-panel/home-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-panel',
+  imports: [],
+  templateUrl: './home-panel.html',
+  styleUrl: './home-panel.scss'
+})
+export class HomePanel {
+
+}
