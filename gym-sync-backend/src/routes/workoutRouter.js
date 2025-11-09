8cc4840a26a198f4c55f4fb1196d59b22c9c5e93
Paweł Heleniak <pawel.heleniak01@gmail.com>
2025-11-10 00:10:06 +0100

    Backend - Usuwanie trening, dodanie komentarza

--- gym-sync-backend/src/routes/workoutRouter.js
+++ gym-sync-backend/src/routes/workoutRouter.js
@@ -4,11 +4,13 @@ import {
   getWorkout,
   getWorkouts,
   deleteWorkout,
+  updateWorkout,
 } from "../controllers/workoutController.js";
 
 const router = express.Router();
 
 router.post("/add", addWorkout);
+router.put("/update/:id", updateWorkout);
 router.get("/:id", getWorkout);
 router.get("/", getWorkouts);
 router.delete("/delete/:id", deleteWorkout);
