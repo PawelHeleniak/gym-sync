c4997e68f0c55f9611cb4ecc70cc49c6cb594b75
Paweł Heleniak <pawel.heleniak01@gmail.com>
2026-02-27 18:33:54 +0100

    Przygotowanie do wystawienia na serwer

--- gym-sync-backend/src/server.js
+++ gym-sync-backend/src/server.js
@@ -5,8 +5,13 @@ import workoutRoutes from "./routes/workoutRouter.js";
 import workoutHistoryRoutes from "./routes/workoutHistoryRouter.js";
 import userRoutes from "./routes/userRouter.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
@@ -15,25 +20,31 @@ app.use(
 );
 
 app.set("trust proxy", 1);
-
 app.use(express.json());
 
-// Routery
 app.use("/workout", workoutRoutes);
 app.use("/history", workoutHistoryRoutes);
 app.use("/auth", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Połączono");
+// === Pod serwerem statyczne pliki z katalogu public_html ===
+const distPath = path.join(__dirname, "../../../public_html");
+
+app.use(express.static(distPath));
+
+app.use((req, res, next) => {
+  if (req.method !== "GET") return next();
+  res.sendFile(path.join(distPath, "index.html"));
 });
+// ===
 
 const PORT = process.env.PORT || 3000;
+
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(PORT, () =>
-      console.log(`Serwer działa na: http://localhost:${PORT}`),
-    );
+    app.listen(PORT, () => {
+      console.log(`Serwer działa na porcie ${PORT}`);
+    });
   } catch (error) {
     console.error("Błąd startu serwera", error.message);
   }
